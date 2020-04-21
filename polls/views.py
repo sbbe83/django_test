9d6db33642c33335c499900399a7822f9dc2dc46
Christopher Ebbs <chrise@vermontsystems.com>
2020-04-21 14:52:19 -0400

    Finishing up tutorial for polls app and commiting.

--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,34 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
-def index(request):
-  latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
 
-def detail(request, question_id):
-  question = get_object_or_404(Question, pk=question_id)
-  return render(request, 'polls/detail.html', {'question': question})
+class IndexView(generic.ListView):
+  template_name = 'polls/index.html'
+  context_object_name = 'latest_question_list'
 
-def results(request, question_id):
-  response = "You're looking at the results of question %s."
-  return HttpResponse(response % question_id)
+  def get_queryset(self):
+    return Question.objects.filter(
+      pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+  model = Question
+  template_name = 'polls/detail.html'
+
+  def get_queryset(self):
+    return Question.objects.filter(pub_date__lte=timezone.now())  
+
+
+class ResultsView(generic.DetailView):
+  model = Question
+  template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
